05643a0bf762527dd057468f288ed7a77ea50d52
MichaelRaatsPro <michaelraatspro@gmail.com>
2024-04-29 16:48:48 +0200

    started the underline priority colour

--- task-manager/src/components/Todo.js
+++ task-manager/src/components/Todo.js
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({ priority ,task, toggleComplete,deleteTodo, editTodo}) => {
+export const Todo = ({ priority ,task, toggleComplete,deleteTodo, editTodo, priorityColor}) => {
   return (
-    <div className = "Todo">
+    <div className = "Todo" style = {{borderBottom: `2px solid ${priorityColor}`  }}>
         <p onClick = {() => toggleComplete(task.id)} className =  {`${task.completed ? 'completed' : ""}`}>[{priority}] {task.task}</p>
         <div style = {{float: 'right'}}>
             <FontAwesomeIcon icon = {faPenToSquare} onClick = {() => editTodo(task.id)}/>
