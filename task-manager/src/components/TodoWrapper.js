4b04d9b9033e2651b150ee699363ca0cd36bbccc
MichaelRaatsPro <michaelraatspro@gmail.com>
2024-04-30 16:50:16 +0200

    got user data to be stored in cookies

--- task-manager/src/components/TodoWrapper.js
+++ task-manager/src/components/TodoWrapper.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react'
+import Cookies from 'js-cookie';
+// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { TodoForm } from './TodoForm'
 import { Todo } from './Todo';
 import {v4 as uuidv4} from 'uuid';
@@ -50,9 +52,26 @@ export const TodoWrapper = () => {
         const sortedTodos = [...todos];
         sortedTodos.sort((a,b) => a.priority - b.priority);
         if (todos.length > 0){
-          setTodos(prevTodos => sortedTodos);
+          setTodos( sortedTodos);
         }
   }
+
+  useEffect(() => {
+    const storedTasks = Cookies.get('todos');
+    if (storedTasks) {
+        setTodos(JSON.parse(storedTasks));
+    }
+}, []);
+
+  useEffect(() => {
+    if (todos.length > 0) {
+      console.log("Current todos: ", todos);
+      const expirationDate = new Date();
+      expirationDate.setFullYear(expirationDate.getFullYear() + 10); // 10 years from now
+      Cookies.set('todos', JSON.stringify(todos), { expires: expirationDate });
+    }
+  }, [todos]);
+
 
   return (
     <div className = 'TodoWrapper'>
